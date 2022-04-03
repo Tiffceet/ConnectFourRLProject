0b2d1a9ada9aeac27ca3dc4e52e3bffe905e5110
Tiffceet <zkang.loo@gmail.com>
2022-04-03 20:57:21 +0800

    Add: tensorboard

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from ConnectFourEnv import ConnectFourEnv
+from gym.wrappers import FlattenObservation
 import gym
 import os
 from stable_baselines3 import A2C
@@ -6,16 +7,24 @@ from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_checker import check_env
 
+# env = VecFrameStack(env, n_stack=4)
+env = ConnectFourEnv()
+env = FlattenObservation(env)
+log_path = os.path.join('Training', 'Logs')
+a2c_path = os.path.join('Training', 'Saved Models', 'C4')
+
 def verifyEnv():
-    env = ConnectFourEnv()
     check_env(env, warn=True)
 
-verifyEnv()
-# env = VecFrameStack(env, n_stack=4)
-# log_path = os.path.join('Training', 'Logs')
-# a2c_path = os.path.join('Training', 'Saved Models', 'C4')
-# model = A2C('CnnPolicy', env, verbose=1, tensorboard_log=log_path)
-# # model = A2C.load(a2c_path, env=env)
-# # print(evaluate_policy(model, env, n_eval_episodes=10, render=True))
-# model.learn(total_timesteps=50000)
-# model.save(a2c_path)
+# verifyEnv()
+
+def train():
+    model = A2C('MlpPolicy', env, verbose=1, tensorboard_log=log_path)
+    model.learn(total_timesteps=50000)
+    model.save(a2c_path)
+
+def load_model():
+    model = A2C.load(a2c_path, env=env)
+    print(model.predict([0]*42))
+
+load_model()
