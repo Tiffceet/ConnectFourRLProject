f766cb608f4a5afba9fb2abc6afeb6d113b0b461
Tiffceet <zkang.loo@gmail.com>
2022-04-03 19:59:43 +0800

    Add: Win condition and player switching

--- ConnectFourEnv.py
+++ ConnectFourEnv.py
@@ -30,14 +30,76 @@ class ConnectFourEnv(gym.Env):
         :returns: int reward - reward value corresponding to the step taken
         :returns: bool done - is the game completed?
         """
-        pass
+        reward = 0
+        done = False
+        if action < 0 or action > 6:
+            raise Exception("Invalid action")
+
+        # Check and perform action
+        for index in list(reversed(range(6))):
+            if self.__board[index][action] == 0:
+                self.__board[index][action] = self.__current_player
+                break
+
+        # Check for draw
+        if np.count_nonzero(self.__board[0]) == 7:
+            reward = 0
+        # Else Check for win
+        elif self.check_win():
+            reward = self.__current_player
+            done = True
+        
+        self.__current_player = -self.__current_player
+        return self.__board, reward, done, {}
+
+    def check_win(self) -> bool:
+        """
+        Checks if the board have any winning players
+        """
+
+        # Test rows
+        for i in range(6):
+            for j in range(7 - 3):
+                value = sum(self.__board[i][j:j + 4])
+                if abs(value) == 4:
+                    return True
+
+        # Test columns on transpose array
+        reversed_board = [list(i) for i in zip(*self.__board)]
+        for i in range(7):
+            for j in range(6 - 3):
+                value = sum(reversed_board[i][j:j + 4])
+                if abs(value) == 4:
+                    return True
+
+        # Test diagonal
+        for i in range(6 - 3):
+            for j in range(7 - 3):
+                value = 0
+                for k in range(4):
+                    value += self.__board[i + k][j + k]
+                    if abs(value) == 4:
+                        return True
+
+        reversed_board = np.fliplr(self.__board)
+        # Test reverse diagonal
+        for i in range(6 - 3):
+            for j in range(7 - 3):
+                value = 0
+                for k in range(4):
+                    value += reversed_board[i + k][j + k]
+                    if abs(value) == 4:
+                        return True
+
+        return False
 
     def reset(self):
         """
         Reset the board
         :returns: numpy.array(6,7) board state after its being reset
         """
-        pass
+        self.__board = np.zeros((6, 7))
+        return self.__board
 
     def render(self):
         """
--- index.py
+++ index.py
@@ -1,4 +1,13 @@
 from ConnectFourEnv import ConnectFourEnv
 
 e = ConnectFourEnv()
+e.step(1)
+e.step(2)
+e.step(1)
+e.step(2)
+e.step(1)
+e.step(2)
+print(e.check_win())
+e.step(1)
+print(e.check_win())
 e.render()
